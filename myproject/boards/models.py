d4b29bb520fcc52ebd4b9c9f672183727cb108ac
Maldonado Bryan L NUWC Newport 2533 <1513324355@mil>
2018-08-17 15:45:35 -0400

    currently on part 3 of the django tutorial.  Added unit testing, bootstrap and links as well as definition of models.

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class Board(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=255)
+    last_updated = models.DateTimeField(auto_now_add=True)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='topics')
+    starter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='topics')
+
+class Post(models.Model):
+    message = models.TextField(max_length=4000)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)# auto_now_add instructs django to get current date and time
+    updated_at = models.DateTimeField(null=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')# using foreign key will 
+                                                                                        #create a propr relationship
+                                                                                        # at the database level.
+    updated_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='+')
--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
+from .models import Board
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk): # pk is sent from the url request and has the board id to get
+    board = get_object_or_404(Board, pk=pk) 
+    return render(request, 'topics.html', {'board':board})
+
