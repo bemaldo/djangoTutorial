770199c5f605d7f3f3517a1ac84fe2794a361e85
Maldonado Bryan L NUWC Newport 2533 <1513324355@mil>
2018-08-21 16:29:12 -0400

    installed django form tweaks library and created some more functionality for the post form.  Modified the look and feel a bit more and adjusted some of the labels to better suit the workflow app.

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from django.urls import reverse, resolve
 from .views import home, board_topics, new_topic
-from .models import Board
+from .models import Board, Topic, Post
 
 class HomeTests(TestCase):
 
@@ -60,13 +60,13 @@ class NewTopicTests(TestCase):
         self.assertContains(response, 'csrfmiddlewaretoken')
 
     def test_new_topic_valid_post_data(self):
-        url = reverse('new_topic', kwargss={'pk': 1})
+        url = reverse('new_topic', kwargs={'pk': 1})
         data = {
-            'subject': 'Test tile',
+            'subject': 'Test title',
             'message': 'Lorem ipsum dolor sit amet'
         }
         response = self.client.post(url, data)
-        self.assertTrue(Topic.object.exists())
+        self.assertTrue(Topic.objects.exists())
         self.assertTrue(Post.objects.exists())
 
     def test_new_topic_invalid_post_data(self):
--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
+from .forms import NewTopicForm
+from .models import Board, Topic, Post
 
 # Create your views here.
 from django.contrib.auth.models import User
@@ -14,27 +17,27 @@ def board_topics(request, pk): # pk is sent from the url request and has the boa
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first() # TODO: get the currently logged in user
 
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
+        form = NewTopicForm(request.POST)
+        
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                    message = form.cleaned_data.get('message'),
+                    topic=topic,
+                    created_by = user
+                )
+        
+            
 
-        user = User.objects.first() # TODO: get the currently logged in user
-
-        topic = Topic.objects.create(
-                subject = subject,
-                board = board,
-                starter = user
-            )
-
-        post = Post.objects.create(
-                message = message,
-                topic = topic,
-                created_by = user
-
-            )
-
-        return redirect('board_topics', pk=board.pk) #TODO: redirect to the created topic page
+            return redirect('board_topics', pk=board.pk) #TODO: redirect to the created topic page
+    else:
+        form = NewTopicForm()
 
 
-    return render(request, 'new_topic.html', {'board':board})
+    return render(request, 'new_topic.html', {'board':board, 'form': form})
